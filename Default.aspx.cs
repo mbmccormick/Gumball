cceb623f63dc0570a36ab8d23ede6affbbfbdbac
Matt McCormick <mbmccormick@gmail.com>
2011-07-12 22:18:54 -0700

    Implemented geocoding.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -21,9 +21,9 @@ namespace Gumball
             else
                 identifier = Request.QueryString["id"];
             
-            var jsonResult = client.DownloadString("http://bubblegum.me/api/v1/timelines/" + identifier);
+            var jsonResult1 = client.DownloadString("http://bubblegum.me/api/v1/timelines/" + identifier);
 
-            foreach (var p in Json.Decode(jsonResult))
+            foreach (var p in Json.Decode(jsonResult1))
             {
                 StringBuilder data = new StringBuilder();
                 data.AppendLine("<div class=\"photo\">");
@@ -39,19 +39,34 @@ namespace Gumball
                 data.AppendLine("</div>");
                 data.AppendLine("<div class=\"photo-data-meta\">");
 
+                var jsonResult2 = client.DownloadString("http://maps.googleapis.com/maps/api/geocode/json?address=" + p.photo.lat + "," + p.photo["long"] + "&sensor=false");
+                var r = Json.Decode(jsonResult2).results[0];
+
+                string city = "";
+                string state = "";
+                foreach (var t in r.address_components)
+                {
+                    if (t.types[0] == "locality")
+                        city = t.long_name;
+                    else if (t.types[0] == "administrative_area_level_1")
+                        state = t.short_name;
+                }
+
+                var location = city + ", " + state;
+                
                 DateTime createdDate = DateTime.ParseExact(p.photo.created_at, "yyyy-MM-ddTHH:mm:ssZ", new System.Globalization.CultureInfo("en-US"));
                 
                 if (DateTime.UtcNow.Subtract(createdDate).Days == 0)
                     if (DateTime.UtcNow.Subtract(createdDate).Hours == 0) 
-                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> just now at " + p.photo.lat + ", " + p.photo["long"] + ".");
+                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> just now in " + location + ".");
                     else if (DateTime.UtcNow.Subtract(createdDate).Hours == 1)
-                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Hours + " hour ago at " + p.photo.lat + ", " + p.photo["long"] + ".");
+                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Hours + " hour ago in " + location + ".");
                     else
-                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Hours + " hours ago at " + p.photo.lat + ", " + p.photo["long"] + ".");
+                        data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Hours + " hours ago in " + location + ".");
                 else if (DateTime.UtcNow.Subtract(createdDate).Days == 1)
-                    data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Days + " day ago at " + p.photo.lat + ", " + p.photo["long"] + ".");
+                    data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Days + " day ago in " + location + ".");
                 else
-                    data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Days + " days ago at " + p.photo.lat + ", " + p.photo["long"] + ".");
+                    data.AppendLine("This photo was taken by <a href=\"http://gumball.apphb.com/?id=" + p.photo.user.id + "\">" + p.photo.user.user_name + "</a> about " + DateTime.UtcNow.Subtract(createdDate).Days + " days ago in " + location + ".");
 
                 data.AppendLine("<br />");
                 data.AppendLine("<br />");
